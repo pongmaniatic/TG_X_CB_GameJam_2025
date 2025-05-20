3415724167126e58ea204f3a95dc7aa04fd2b7b8
eliasrbennett <33009276+eliasrbennett@users.noreply.github.com>
2025-05-20 13:31:30 -0700

    created cloud particle effect. still working on polish

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] private float moveSpeed;
+
+    [SerializeField] private float lifetimeMultiplier;
+
+    [SerializeField] private ParticleSystem ps;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 moveInput = InputSystem.actions.FindAction("move").ReadValue<Vector2>();
+        if (moveInput.magnitude != 0)
+        {
+            transform.position += new Vector3(moveInput.x, moveInput.y, 0) * Time.deltaTime * moveSpeed;
+            var main = ps.main;
+            main.startLifetimeMultiplier = lifetimeMultiplier;
+        }
+        else
+        {
+            var main = ps.main;
+            main.startLifetimeMultiplier = 2;
+        }
+    }
+}
