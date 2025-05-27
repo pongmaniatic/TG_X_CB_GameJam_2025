219c3b6e7ed9401bddba96af433840c3ac55aa5a
Esteban Morales <pongmaniatic@gmail.com>
2025-05-27 14:03:07 -0700

    minor fix to plant appearing

--- Assets/Scripts/FlowerBehaviour.cs
+++ Assets/Scripts/FlowerBehaviour.cs
@@ -1,5 +1,6 @@
 using UnityEngine;
 using UnityEngine.Events;
+using System.Collections;
 
 public class FlowerBehaviour : MonoBehaviour
 {
@@ -22,11 +23,43 @@ public class FlowerBehaviour : MonoBehaviour
         {
             if (collision.gameObject.CompareTag("Water"))
             {
-                Destroy(collision.gameObject);
-                flowerColorUntility.PaintHealthyFlower(gameObject);
                 flowerIsDry = false;
+                Destroy(collision.gameObject);
+                StartCoroutine(FlowerTranformFromDryToHealthy());
             }
         }
+    }
+
+
+    IEnumerator FlowerTranformFromDryToHealthy()
+    {
+        float timeElapsed       = 0f;
+        Vector3 initialScale    = new Vector3(2, 1, 1);//splashSprite.transform.localScale;
+        Vector3 targetScale     = new Vector3(0.01f, 0.01f, 0.01f); // Final scale
+        float scaleDuration     = 0.3f; // Time in seconds before destruction
+
+
+        while (timeElapsed < scaleDuration)
+        {
+
+            gameObject.transform.localScale = Vector3.Lerp(initialScale, targetScale, timeElapsed / scaleDuration);
+            timeElapsed += Time.deltaTime;
+            yield return null;
+        }
+
+        flowerColorUntility.PaintHealthyFlower(gameObject);
+        timeElapsed = 0f;
+
+        while (timeElapsed < scaleDuration)
+        {
+            Debug.Log(timeElapsed);
+            gameObject.transform.localScale = Vector3.Lerp(targetScale, initialScale, timeElapsed / scaleDuration );
+            timeElapsed += Time.deltaTime;
+            yield return null;
+        }
+
+        //gameObject.transform.localScale = targetScale;
+
     }
 
 }
